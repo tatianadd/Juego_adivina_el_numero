32b7d7849e7a56fa486c9a6013c98ce4dea623bf
tatianadd <tatiana.s.diaz.duque@gmail.com>
2025-09-02 14:57:47 -0400

    Add files via upload

---
+++ Juego adivina numero/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Juego_adivina__numero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Nro_Intentos = new System.Windows.Forms.Label();
+            this.Lista_Intentos = new System.Windows.Forms.ListBox();
+            this.CajaTexto = new System.Windows.Forms.TextBox();
+            this.NuevoJuego = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 118);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Probar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Nro_Intentos
+            // 
+            this.Nro_Intentos.AutoSize = true;
+            this.Nro_Intentos.Location = new System.Drawing.Point(272, 148);
+            this.Nro_Intentos.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Nro_Intentos.Name = "Nro_Intentos";
+            this.Nro_Intentos.Size = new System.Drawing.Size(35, 13);
+            this.Nro_Intentos.TabIndex = 1;
+            this.Nro_Intentos.Text = "label1";
+            // 
+            // Lista_Intentos
+            // 
+            this.Lista_Intentos.FormattingEnabled = true;
+            this.Lista_Intentos.Location = new System.Drawing.Point(248, 172);
+            this.Lista_Intentos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Lista_Intentos.Name = "Lista_Intentos";
+            this.Lista_Intentos.Size = new System.Drawing.Size(91, 69);
+            this.Lista_Intentos.TabIndex = 2;
+            // 
+            // CajaTexto
+            // 
+            this.CajaTexto.Location = new System.Drawing.Point(230, 77);
+            this.CajaTexto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CajaTexto.Name = "CajaTexto";
+            this.CajaTexto.Size = new System.Drawing.Size(121, 20);
+            this.CajaTexto.TabIndex = 3;
+            // 
+            // NuevoJuego
+            // 
+            this.NuevoJuego.Location = new System.Drawing.Point(230, 254);
+            this.NuevoJuego.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.NuevoJuego.Name = "NuevoJuego";
+            this.NuevoJuego.Size = new System.Drawing.Size(130, 19);
+            this.NuevoJuego.TabIndex = 4;
+            this.NuevoJuego.Text = "Nuevo juego";
+            this.NuevoJuego.UseVisualStyleBackColor = true;
+            this.NuevoJuego.Click += new System.EventHandler(this.NuevoJuego_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Adivina el número secreto entre 1 y 100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NuevoJuego);
+            this.Controls.Add(this.CajaTexto);
+            this.Controls.Add(this.Lista_Intentos);
+            this.Controls.Add(this.Nro_Intentos);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Nro_Intentos;
+        private System.Windows.Forms.ListBox Lista_Intentos;
+        private System.Windows.Forms.TextBox CajaTexto;
+        private System.Windows.Forms.Button NuevoJuego;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Juego adivina numero/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juego_adivina__numero
+{
+    public partial class Form1 : Form
+    {
+        // Declaramos las variables que vamos a utilizar
+        private int numeroSecreto;  // Variable para guardar el número secreto que el jugador debe adivinar
+        private int intentos;       // Variable para contar la cantidad de intentos realizados
+        private Random aleatorio;   // Instancia de la clase Random para generar números aleatorios
+
+        // Constructor de la clase Form1, se ejecuta al iniciar la aplicación
+        public Form1()
+        {
+            InitializeComponent();    // Inicializa los componentes del formulario (interfaz gráfica)
+            aleatorio = new Random(); // Crea un nuevo generador de números aleatorios
+            IniciarJuego();           // Llama al método que inicia el juego
+        }
+
+        // Método para iniciar el juego (se ejecuta cada vez que se reinicia el juego)
+        private void IniciarJuego()
+        {
+            CajaTexto.Enabled = true;  // Habilita la caja de texto para que el jugador pueda ingresar un número
+            button1.Enabled = true;    // Habilita el botón para hacer la adivinanza
+            Lista_Intentos.Items.Clear();  // Limpia la lista de intentos previos
+            CajaTexto.Clear();            // Limpia la caja de texto
+            numeroSecreto = aleatorio.Next(1, 101); // Genera un número aleatorio entre 1 y 100 (ambos incluidos)
+            intentos = 0; // Resetea el contador de intentos a 0
+            Nro_Intentos.Text = "Intentos: 0"; // Muestra en el label la cantidad de intentos realizados (inicia en 0)
+        }
+
+        // Este método se ejecuta cuando el jugador hace clic en el botón de adivinar
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Intentamos convertir el texto ingresado a un número entero
+            if (int.TryParse(CajaTexto.Text, out int numeroIngresado))
+            {
+                // Aumentamos el contador de intentos
+                intentos++;
+
+                // Actualizamos el label para mostrar el número actual de intentos
+                Nro_Intentos.Text = "Intentos: " + intentos;
+
+                // Añadimos el número ingresado a la lista de intentos
+                Lista_Intentos.Items.Add(numeroIngresado);
+
+                // Verificamos si el número ingresado es el número secreto
+                if (numeroIngresado == numeroSecreto)
+                {
+                    // Si el jugador adivinó correctamente, mostramos un mensaje de felicitación
+                    MessageBox.Show("¡Felicidades! Adivinaste el número secreto en " + intentos + " intentos.");
+                    CajaTexto.Enabled = false; // Deshabilita la caja de texto para evitar más intentos
+                    button1.Enabled = false;   // Deshabilita el botón de adivinar
+                }
+                else if (numeroIngresado < numeroSecreto)
+                {
+                    // Si el número ingresado es menor que el número secreto, mostramos un mensaje
+                    MessageBox.Show("El número secreto es mayor.");
+                }
+
+                else if (numeroIngresado > 100)
+                {
+                    MessageBox.Show("El numero ingresado es mayor que 100, ingrese un numero valido");
+                }
+
+                else
+                {
+                    // Si el número ingresado es mayor que el número secreto, mostramos otro mensaje
+                    MessageBox.Show("El número secreto es menor.");
+                }
+
+                // Limpiamos la caja de texto para el siguiente intento
+                CajaTexto.Clear();
+
+                // Volvemos a poner el foco en la caja de texto
+                CajaTexto.Focus();
+            }
+            else
+            {
+                // Si el texto ingresado no es un número válido, mostramos un mensaje de error
+                MessageBox.Show("Por favor, ingresa un número válido.");
+
+
+
+
+            }
+        }
+
+        // Este método se ejecuta cuando el jugador hace clic en el botón "Nuevo Juego"
+        private void NuevoJuego_Click(object sender, EventArgs e)
+        {
+            // Llama al método IniciarJuego para reiniciar el juego con un nuevo número secreto
+            IniciarJuego();
+        }
+
+        // Este método está vacío y no se utiliza, es probable que sea un evento no implementado
+        private void Nro_Intentos_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
